75cb965735688bb34baf8d68eb2c562fe14a85ff
abdulmalikgh <50835233+abdulmalikgh@users.noreply.github.com>
2019-11-13 16:14:57 -0800

    completed doctor login validation

---
+++ Doctor/js/doctorLogin.js
@@ -0,0 +1,52 @@
+
+const validateEmail = (emailInput)=> {
+ const regex = /[@.]/;
+ if(regex.test(emailInput)){
+     return true;
+ } else {
+     return false;
+ }
+}
+
+document.doctorLogin.register.addEventListener('click',(event)=>{
+const email = document.doctorLogin.email.value;
+const password = document.doctorLogin.password.value;
+const emailError = document.querySelector('.emailError');
+const passwordError = document.querySelector('.passError')
+    event.preventDefault(); 
+    let errorCounter = 0;
+
+    if(email === ""){
+        emailError.textContent = "Please Enter Your Email";
+        document.doctorLogin.email.classList.add('isInvalid');
+        errorCounter++;
+    } else if(!validateEmail(email)){
+    emailError.textContent = "Email format is invalid e.g me@example.com";
+    document.doctorLogin.email.classList.add('isInvalid');
+    errorCounter++;  
+}
+ else{
+        emailError.textContent =""; 
+        document.doctorLogin.email.classList.remove('isInvalid');
+  }
+
+  if(password === "") {
+      passwordError.textContent = "Please enter your password";
+      document.doctorLogin.password.classList.add('isInvalid');
+    errorCounter++; 
+  } else if(password.length < 8){
+   passwordError.textContent = "password should be greater than 8 characters";
+   document.doctorLogin.password.classList.add('isInvalid');
+   errorCounter++; }
+  else{
+    passwordError.textContent = "";
+    document.doctorLogin.password.classList.remove('isInvalid');
+  }
+
+
+  // sending to api
+  if(errorCounter == 0) alert('succesful login')
+});
+
+
+
